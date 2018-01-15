2594cf332b3813d3898c81b4d1a3ed6a81211676
TinnyGit <tinny.gidenskold@gmail.com>
2018-01-15 19:39:17 +0100

    Light controller

---
+++ lightSensor2018.ino
@@ -0,0 +1,40 @@
+#include <elapsedMillis.h>
+
+int sensorPin = A0;     // 
+int relayPin = 7;      // Connected Relay
+int sensorValue = 0;   // Connected Sensor 
+int alarmTriggered = 600;
+int controlValue = 0;
+
+elapsedMillis timer;
+
+void setup() {
+  pinMode(relayPin, OUTPUT);
+}
+void loop() {
+  ifVilkor();
+}
+
+//-------------Functions-------------//
+void ifVilkor(){
+  sensorValue = analogRead(sensorPin);
+
+  if (sensorValue > alarmTriggered) {
+    lightOn();
+    resetTimer();
+    controlValue = 1;
+  }
+  if ((timer > 60000) && ( controlValue == 1 )) {
+    lightOff();
+    controlValue = 0;
+  }
+  }
+void resetTimer() {
+  timer = 0;
+}
+void lightOn() {
+  digitalWrite(relayPin, HIGH);
+}
+void lightOff() {
+  digitalWrite(relayPin, LOW);
+}
